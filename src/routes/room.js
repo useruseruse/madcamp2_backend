fa9e2e4fac65276105664083c77ecf2ba758bc30
tylerkim1 <tylerkim129@gmail.com>
2023-07-11 11:55:58 +0000

    asdklfj

--- src/routes/room.js
+++ src/routes/room.js
@@ -28,15 +28,29 @@ router.post('/add', async (req, res) => {
         const roomMinPpl = room.roomMinPpl
         const roomMaxPpl = room.roomMaxPpl
         const isStart = room.isStart
-        console.log(`room : ${room}`)
-        console.log(typeof users)
-        console.log(`title : ${users}`)
-        
-        const newRoom = new RoomModel({roomId, users, roomTitle, roomMode, roomMinPpl, roomMaxPpl, isStart});
+    
+        const newRoom = await new RoomModel({roomId, users, roomTitle, roomMode, roomMinPpl, roomMaxPpl, isStart}).save();
         console.log(`title : ${newRoom}`)
-        const result = await newRoom.save()
-        if (!result) return res.status(404).send({ err: 'Cannot Add Room' });
-        else return res.status(200).json({ isOK: true });
+
+        const updatedRoom = await RoomModel.findByIdAndUpdate(
+            newRoom._id,
+            { roomId: newRoom._id },
+            { new: true }
+        );
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            users[0].userId, 
+            {currentRoom: newRoom._id},
+            {new: true}
+        );
+        console.log(users[0].userId, newRoom._id)
+
+        if (!updatedRoom) return res.status(404).send({ err: 'Cannot Add Room' });
+        else {
+            console.log("updated", updatedRoom);
+            return res.status(200).json(updatedRoom);
+        }
+
+
     }catch(err){
         return res.status(500).send(err);
     }
@@ -46,14 +60,17 @@ router.post('/add', async (req, res) => {
 router.post('/enter', async (req, res) => {
     try {
         const roomAndUser = req.body;
-        const room = roomAndUser.room
+        const roomId = roomAndUser.roomId
         const user = roomAndUser.user
-        const update_currRoom = await UserModel.updateOne({id: user.id}, {currentRoom: room.id})
-        if(!update_currRoom) return res.status(404).json("cannot update user's current room ")
+        const update_currRoom = await UserModel.updateOne({userId: user.userId}, {currentRoom: roomId})
+        if(!update_currRoom) return res.status(444).json("cannot update user's current room ")
 
-        const users = room.users.push(user)
-        const update_roomUsers = await RoomModel.updateOne({id: room.id}, {users: users})
-        if(!update_roomUsers) return res.status(404).json("cannot update users in room ")
+        const room = await RoomModel.findOne({ roomId: roomId });
+        const added_user = await UserModel.findOne({ userId: user.userId });
+        room.users.push(added_user); // users 배열에 added_user 추가
+        const update_roomUsers = await RoomModel.updateOne({ roomId: roomId }, { users: room.users }); // users 필드에 전체 배열 할당
+
+        if(!update_roomUsers) return res.status(454).json("cannot update users in room ")
 
         return res.status(200).json(true);
     } catch (err) {
@@ -66,26 +83,26 @@ router.post('/leave', async (req, res) => {
     try {
         const { user } = req.body;
         const curr_room_id = user.currentRoom
-        const curr_room = await RoomModel.findOne({id: curr_room_id})
+        const curr_room = await RoomModel.findOne({roomId: curr_room_id})
         if (!curr_room) {
-            return res.status(200).json("user is not in any room");
+            return res.status(444).json("user is not in any room");
         }
         else if(curr_room.users.length == 1){       //남은 유저가 한명이면 방 폭파 
-            const update_currRoom = await UserModel.updateOne({id: user.id},{currentRoom: -1})
-            if(update_currRoom) return res.status(404).json("user cannot leave a room ")
+            const update_currRoom = await UserModel.updateOne({userId: user.userId},{currentRoom: -1})
+            if(update_currRoom) return res.status(454).json("user cannot leave a room ")
 
-            const delete_room = await RoomModel.deleteOne({id: curr_room_id})          
-            if (delete_room) return res.status(404).json("room is not properly deleted")
+            const delete_room = await RoomModel.deleteOne({roomId: curr_room_id})          
+            if (delete_room) return res.status(464).json("room is not properly deleted")
             
             return res.status(200).json("user properly leaved room and room deleted")
         }
         else{                                       //남은 유저가 여러명이면 방의 유저 변경 
-            const update_currRoom = await UserModel.updateOne({id: user.id},{currentRoom: -1})
-            if (!update_currRoom) return res.status(404).json("room is not properly updated")
+            const users = curr_room.users.filter((e) => e.userId !== user.userId);
+            const update_roomUsers = await RoomModel.updateOne({roomId: curr_room_id},{users: users})
+            if(!update_roomUsers) return res.status(474).json("user did not leave the room")
 
-            const users = curr_room.users.filter((e) => e !== user);
-            const update_roomUsers = await RoomModel.updateOne({id: curr_room_id},{users: users})
-            if(!update_roomUsers) return res.status(404).json("user did not leave the room")
+            const update_currRoom = await UserModel.updateOne({userId: user.userId},{currentRoom: -1})
+            if (!update_currRoom) return res.status(484).json("room is not properly updated")
 
            return res.status(200).json("user properly leaved room and room updated")
         }
